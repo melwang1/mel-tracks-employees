42f43b51661bd6c08f0bafc737ae36ebaf869a8f
Melanie Wang <melaniiewang@hotmail.com>
2023-07-12 12:19:12 -0400

    final push to ensure everything is up to date

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const questions = [
         name: "text",
         message: "What would you like to do?",
         choices: ['View all Employees', 'Add Employee', 'Update Employee Role', 'View all Roles', 'Add Role', "View all Departments", 'Add Department']
-
+        
     },
 
     {
@@ -213,58 +213,3 @@ function addEmployee() {
         })
     })
 }
-
-//     {
-//         type: "input",
-//         name: "text",
-//         message: "What is the name of the role?"
-//     }, 
-
-//     {
-//         type: "input",
-//         name: "text",
-//         message: "What is the salery of the role?"
-//     }, 
-
-//     {
-//         type: "list",
-//         name: "text",
-//         message: "Which department does the role belong to?",
-//         choices: ['Engineering', 'Finance', 'Legal', 'Sale', 'Service']
-
-//     },
-
-//     {
-//         type: "list",
-//         name: "text",
-//         message: "What would you like to do?",
-//         choices: ['View all Employees', 'Add Employee', 'Update Employee Role', 'View all Roles', 'Add Role', "View all Departments", 'Add Department']
-
-//     },
-
-//     {
-//         type: "input",
-//         name: "text",
-//         message: "What is the employee's first name?"
-//     }, 
-
-//     {
-//         type: "input",
-//         name: "text",
-//         message: "What is the employee's last name?"
-//     }, 
-
-//     {
-//         type: "list",
-//         name: "text",
-//         message: "What is the employee's role?",
-//         choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', "Accountant", 'Legal Team Lead', 'Lawyer', 'Customer Service']
-//     }, 
-
-//     {
-//         type: "list",
-//         name: "text",
-//         message: "What is the employee's manger?",
-//         choices: []
-//     }, 
-
